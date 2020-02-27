2d51e976efbe953a4d7daf5c984581ac49aa237a
Eli Uriegas <eliuriegas@fb.com>
2020-02-26 16:07:27 -0800

    Switch pruner to check all platforms we support
    Signed-off-by: Eli Uriegas <eliuriegas@fb.com>

--- prune.sh
+++ prune.sh
@@ -1,36 +1,35 @@
 #!/usr/bin/env bash
 
+grab_version() {
+    conda search -c "${CHANNEL}" --platform "${platform}" "${PKG}" 2>/dev/null | \
+        grep "${CHANNEL}" | \
+        awk -F '  *' '{print $2}' | \
+        uniq | \
+        head -n -1 | \
+        xargs
+}
+
 # TODO: Write this in a proper programming language
 
 set -eou pipefail
 
 CHANNEL=${CHANNEL:-pytorch-nightly}
 PKG=${PKG:-pytorch}
+PLATFORMS=${PLATFORMS:-noarch osx-64 linux-64 win-64}
 
-# TODO: Refactor this to cover all platforms we currently support
-#       * osx-64
-#       * linux-64
-#       * win-64
 # Will grab all versions except for the latest one,
-versions_to_prune=$(\
-    conda search -c "${CHANNEL}" "${PKG}" | \
-        grep "${CHANNEL}" | \
-        awk -F '  *' '{print $2}' | \
-        uniq | \
-        head -n -1 | \
-        xargs
-)
+versions_to_prune=()
+for platform in ${PLATFORMS}; do
+    versions_to_prune+=( $(grab_version || true) )
+done
 
-if [[ -z "${versions_to_prune}" ]]; then
-    # This might a good thing or a bad thing, who knows?
-    #
-    # Exit gracefully though, since it could just mean there's no
-    # packages up
-    echo "WARNING: Could not find any versions to prune, exiting..."
-    exit 0
-fi
+declare -A unique_versions
+# Only use unique versions
+for version in ${versions_to_prune[@]}; do
+    unique_versions["${version}"]=""
+done
 
-for version in ${versions_to_prune}; do
+for version in ${!unique_versions[@]}; do
     (
         set -x
         anaconda remove --force ${CHANNEL}/${PKG}/${version}
